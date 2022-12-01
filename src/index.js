6d754fd15d119094e198a3f697f1326270c6f074
Vincent Destrait <vincent.destrait@contentsquare.com>
2022-12-01 23:49:18 +0100

    Add data binding and style to the basic oscillator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const MIN_AUDIO_RANGE = 27; // in Hz => A0 (27.5)
-const MAX_AUDIO_RANGE = 4186; // in Hz => C8
-const MIN_DETUNE_RANGE = -1200; // in cents
-const MAX_DETUNE_RANGE = 1200; // in cents
+const MIN_AUDIO_RANGE = 55; // in Hz => A1
+const MAX_AUDIO_RANGE = 1760; // in Hz => A6
+const MIN_DETUNE_RANGE = -100; // in cents
+const MAX_DETUNE_RANGE = 100; // in cents
 const NOTE_A = 440; // in Hz
 
 const playStopButton = new Nexus.TextButton("#playStopButton");
@@ -13,13 +13,14 @@ document
 
 const oscillatorRack = new Nexus.Rack("#oscillator");
 
-oscillatorRack.oscWaveFormSelect.options = [
+oscillatorRack.oscWaveFormSelect.defineOptions([
   "sine",
   "square",
   "sawtooth",
   "triangle",
-];
+]);
 oscillatorRack.oscWaveFormSelect.value = "sine";
+oscillatorRack.oscWaveFormSelect.colorize("fill", "#d4d4d4");
 
 oscillatorRack.oscFreqDial.min = MIN_AUDIO_RANGE;
 oscillatorRack.oscFreqDial.max = MAX_AUDIO_RANGE;
@@ -27,15 +28,20 @@ oscillatorRack.oscFreqDial.value = NOTE_A;
 oscillatorRack.oscFreqDial.step = 1;
 oscillatorRack.oscFreqNumber.link(oscillatorRack.oscFreqDial);
 oscillatorRack.oscFreqNumber.link(oscillatorRack.oscFreqDial);
+oscillatorRack.oscFreqDial.colorize("fill", "#d4d4d4");
+oscillatorRack.oscFreqNumber.colorize("fill", "#d4d4d4");
 
 oscillatorRack.oscDetuneDial.min = MIN_DETUNE_RANGE;
 oscillatorRack.oscDetuneDial.max = MAX_DETUNE_RANGE;
 oscillatorRack.oscDetuneDial.value = 0;
 oscillatorRack.oscDetuneDial.step = 1;
 oscillatorRack.oscDetuneNumber.link(oscillatorRack.oscDetuneDial);
+oscillatorRack.oscDetuneDial.colorize("fill", "#d4d4d4");
+oscillatorRack.oscDetuneNumber.colorize("fill", "#d4d4d4");
 
 oscillatorRack.oscMuteButton.mode = "toggle";
 oscillatorRack.oscMuteButton.state = "true";
+oscillatorRack.oscMuteButton.colorize("fill", "#d4d4d4");
 
 const oscillator = new Tone.Oscillator({
   frequency: oscillatorRack.oscFreqDial.value,
@@ -45,6 +51,18 @@ const oscillator = new Tone.Oscillator({
   .toDestination()
   .start();
 
+oscillatorRack.oscWaveFormSelect.on("change", (option) => {
+  oscillator.type = option.value;
+});
+
+oscillatorRack.oscFreqDial.on("change", (value) => {
+  oscillator.frequency.setValueAtTime(value, Tone.getContext().currentTime);
+});
+
+oscillatorRack.oscDetuneDial.on("change", (value) => {
+  oscillator.detune.setValueAtTime(value, Tone.getContext().currentTime);
+});
+
 oscillatorRack.oscMuteButton.on("change", (state) => {
   if (state) {
     oscillator.mute = true;
@@ -52,6 +70,3 @@ oscillatorRack.oscMuteButton.on("change", (state) => {
     oscillator.mute = false;
   }
 });
-
-const oscilloscope = new Nexus.Oscilloscope("#oscilloscope");
-oscilloscope.connect(oscillator);
